14bcece5319d9eb6fd8fee4fbccd438fb5c29b1c
unknown <oluwaseunadams17@gmail.com>
2023-01-04 19:03:14 +0100

    database connection with mongoose

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDb = async () => {
+  mongoose.set('strictQuery', true);
+  const conn = await mongoose.connect(
+    process.env.MONGO_URI
+  );
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
+
+module.exports = connectDb;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDb = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDb();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,9 +24,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
