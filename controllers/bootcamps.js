5af5e2162548dbd4f0aa8a00ae2c164f5ef29f22
unknown <oluwaseunadams17@gmail.com>
2023-01-12 06:34:17 +0100

    created pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,7 +14,12 @@ exports.getBootcamps = asyncHandler(
     const reqQuery = { ...req.query };
 
     // Fields to exclude
-    const removeFields = ['select', 'sort'];
+    const removeFields = [
+      'select',
+      'sort',
+      'page',
+      'limit',
+    ];
 
     // Loop over removeFields and delete them from reqQuery
     removeFields.forEach((param) => delete reqQuery[param]);
@@ -45,12 +50,39 @@ exports.getBootcamps = asyncHandler(
       query = query.sort('-createdAt');
     }
 
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
     // Executing query
     const bootcamps = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      };
+    }
 
     res.status(200).json({
       success: true,
       count: bootcamps.length,
+      pagination,
       data: bootcamps,
     });
   }
