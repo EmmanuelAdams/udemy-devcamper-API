34762bd3f26ae7a045b2f158d0582fbd2666a880
unknown <oluwaseunadams17@gmail.com>
2023-01-19 22:03:54 +0100

    update and delete reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,8 @@ const {
   getReviews,
   getReview,
   addReview,
+  updateReview,
+  deleteReview,
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -26,6 +28,14 @@ router
   )
   .post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview);
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(
+    protect,
+    authorize('user', 'admin'),
+    deleteReview
+  );
 
 module.exports = router;
