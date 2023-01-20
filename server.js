a2bb0b144477a99c2f0acf71ca889da84f66afb3
unknown <oluwaseunadams17@gmail.com>
2023-01-20 10:00:04 +0100

    hpp and rate limiting

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const colors = require('colors');
 const errorHandler = require('./middleware/error');
 const connectDb = require('./config/db');
@@ -48,6 +51,20 @@ app.use(helmet());
 
 // Prevent xss attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http params pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
